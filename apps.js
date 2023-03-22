3aca0ea0874a951d8f9bbc4b3d583d15b443f0ab
gabyynaira <gabyynaira@gmail.com>
2023-03-22 16:09:50 -0300

    variables

---
+++ apps.js
@@ -0,0 +1,57 @@
+//palabra reservada, nombre, asignacion y el valor;
+
+//tipos de datos primitivos
+
+//Number
+
+const edad = 25;
+const altura = 1.63;
+const negativo = -10;
+
+//String
+
+//const nombre = alba;
+//const apellido = almiron ; 
+//const compañera = hanna;
+
+//console.log(typeof true)
+
+//console.log (typeof edad)
+
+
+//const ejemplo1 = 2;
+//console.log (typeof ("2"))
+
+//console.log (Number ("2") + 1 )
+
+
+//const primerNumero = prompt ("ingresa el primer numero!");
+
+//const segundoNumero = prompt ("ingresa el segundo numero");  
+
+//console.log (Number (primerNumero))+(Number (segundoNumero))
+
+//console.log (Number (primerNumero) + Number (segundoNumero))
+
+//console.log (true)
+
+//Operadores aritmeticos
+
+// + , - , * , / , % , * . 10 , <> , >= , <=
+
+//Operadores de asignacion
+
+// =
+
+//const nombre = prompt("ingresa tu nombre") ;
+
+//alert ("hola " +  nombre  + " bienvenid@"); 
+
+console.log ("este es el normal");
+
+console.warn ("advertencia no pegues codigo aqui :/"); 
+
+console.error ("el usuario ya se ha registrado anteriormente, por favor comunicate con RH ");
+
+
+
